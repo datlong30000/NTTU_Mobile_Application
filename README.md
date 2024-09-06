b964a5444e0bed575852184f86e1b5f20414e5ff
datlong30000 <kaisinyru@gmail.com>
2024-09-06 23:21:37 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# nienproject
+
+A Flutter project for ChatBot handler API.
+
+## Getting Started
+
+Ứng dụng hỗ trợ tuyển sinh đại học dựa trên tập dữ liệu trường Nguyễn Tất Thành.
+
+Một số nguồn kiến thức bạn nên biết trước khi chạy project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+Nếu cần trợ giúp hãy liên hệ Zalo 0788588178 để có thể cùng học tập và phát triển ứng dụng.
---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/kaisinyru/Public/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/kaisinyru/Downloads/chatbot_TVTS/nienproject" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/kaisinyru/Public/flutter"
+  "PROJECT_DIR=/home/kaisinyru/Downloads/chatbot_TVTS/nienproject"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/kaisinyru/Downloads/chatbot_TVTS/nienproject/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/kaisinyru/Downloads/chatbot_TVTS/nienproject/lib/main.dart"
+)
